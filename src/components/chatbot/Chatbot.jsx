bc22c8775b1b699d9674a5ffa12e0f6dc04aebe4
Subhasis Maity <subhasis.maity@fedev.cbnits.com>
2024-04-17 13:03:04 +0530

    chat window design change

--- src/components/chatbot/Chatbot.jsx
+++ src/components/chatbot/Chatbot.jsx
@@ -6,6 +6,7 @@ import {
   Divider,
   IconButton,
   Input,
+  Chip,
 } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 import { styled } from "@mui/material/styles";
@@ -52,13 +53,29 @@ const Chatbot = ({ onClose }) => {
   const [messages, setMessages] = useState([]);
   const bottomRef = useRef(null);
   const [isTyping, setIsTyping] = useState(false);
+  const [isAccepted, setIsAccepted] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(true);
 
   useEffect(() => {
     setMessages([
       {
         text: dataset[0].answer,
         user: false,
-        menulist: [],
+        menulist: [
+          {
+            title: "Hire a Tutor",
+            url: "https://www.connectbud.com/parent-login",
+          },
+          {
+            title: "Become a Tutor",
+            url: "https://www.connectbud.com/tutor-login",
+          },
+          {
+            title: "Book A Free Demo Class",
+            url: "https://www.connectbud.com/courses?title=Coding",
+          },
+          { title: "Have Other Queries", url: "" },
+        ],
         delivered_at: Date.now(),
       },
     ]);
@@ -70,8 +87,7 @@ const Chatbot = ({ onClose }) => {
     }
   }, [messages]);
 
-  const askBot = async (e) => {
-    e.preventDefault();
+  const askBot = async (userInput) => {
     if (!userInput.trim()) return;
 
     console.log("userInput", userInput);
@@ -101,7 +117,7 @@ const Chatbot = ({ onClose }) => {
             setIsTyping(false);
             setMessages((prev) => [...prev, botMsg]);
           }, 2000); // Adjust the duration of typing animation as needed
-        }, 100); // Delay before typing animation starts
+        }, 10); // Delay before typing animation starts
 
         return () => clearTimeout(typingTimer);
       })
@@ -116,128 +132,167 @@ const Chatbot = ({ onClose }) => {
     setUserInput("");
   };
 
+  const onQuerySelection = (item) => {
+    if (item?.title === "Have Other Queries") {
+      setIsDisabled(false);
+      askBot(item?.title);
+    } else {
+      window.open(item?.url);
+    }
+  };
+
   return (
-    <Box className="chatbot-container">
-      {/* header section */}
-      <Box className="header-container">
-        <Box className="header-profile">
-          <StyledBadge
-            overlap="circular"
-            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-            variant="dot"
+    <>
+      <Box className="chatbot-container">
+        {/* header section */}
+        <Box className="header-container">
+          <Box className="header-profile">
+            <StyledBadge
+              overlap="circular"
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              variant="dot"
+            >
+              <Avatar src={ConnectBud} />
+            </StyledBadge>
+            <Box>
+              <Typography
+                color={"#fff"}
+                ml={2}
+                fontFamily={"sans-serif"}
+                fontWeight={550}
+              >
+                ConnectBud Assistant
+              </Typography>
+              <Typography
+                color={"#fff"}
+                ml={2}
+                fontFamily={"sans-serif"}
+                fontSize={12}
+              >
+                Online
+              </Typography>
+            </Box>
+          </Box>
+          <CloseIcon
+            sx={{
+              padding: "10px",
+              cursor: "pointer",
+              marginRight: "10px",
+              color: "#fff",
+            }}
+            onClick={onClose}
+          />
+        </Box>
+
+        {/* chat body */}
+        <Box className="chat-body">
+          <Box
+            id="chat-window"
+            sx={{
+              padding: "20px",
+              margin: "5px",
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+            }}
           >
-            <Avatar src={ConnectBud} />
-          </StyledBadge>
-          <Box>
-            <Typography
-              color={"#fff"}
-              ml={2}
-              fontFamily={"sans-serif"}
-              fontWeight={550}
-            >
-              ConnectBud Assistant
-            </Typography>
-            <Typography
-              color={"#fff"}
-              ml={2}
-              fontFamily={"sans-serif"}
-              fontSize={12}
-            >
-              Online
-            </Typography>
+            {messages.map((msg, index) => (
+              <>
+                <div
+                  key={index}
+                  style={{
+                    display: "flex",
+                    width: msg.user ? "" : "fit-content",
+                    justifyContent: msg.user ? "flex-end" : "flex-start",
+                    gap: 8,
+                  }}
+                >
+                  {!msg.user ? (
+                    <Avatar
+                      src={BotIcon}
+                      sx={{
+                        height: 25,
+                        width: 25,
+                        border: "0.5px solid black",
+                        boxShadow: "2px 5px 10px -1px rgba(0, 0, 0, 1)",
+                        marginTop: "15px",
+                      }}
+                    />
+                  ) : null}
+                  <div>
+                    <Typography
+                      fontSize={14}
+                      className={msg.user ? "user-msg" : "bot-msg"}
+                    >
+                      {msg.text}
+                    </Typography>
+                    {msg?.menulist?.length ? (
+                      <Box
+                        mt={2}
+                        display={"flex"}
+                        flexDirection={"column"}
+                        gap={1}
+                      >
+                        {msg?.menulist?.map((item, index) => (
+                          <Chip
+                            key={index}
+                            label={item?.title}
+                            // variant="outlined"
+                            // color="success"
+                            onClick={() => onQuerySelection(item)}
+                            sx={{
+                              backgroundColor: "darkslategrey",
+                              color: "#fff",
+                              ":hover": { color: "#000" },
+                            }}
+                          />
+                        ))}
+                      </Box>
+                    ) : null}
+                    <Typography
+                      // mt={1}
+                      fontSize={12}
+                      fontWeight={550}
+                      style={{ textAlign: msg.user ? "end" : "start" }}
+                    >
+                      {moment(msg.delivered_at).format("LT")}
+                    </Typography>
+                  </div>
+                </div>
+                <div ref={bottomRef}></div>
+              </>
+            ))}
           </Box>
         </Box>
-        <CloseIcon
-          sx={{
-            padding: "10px",
-            cursor: "pointer",
-            marginRight: "10px",
-            color: "#fff",
-          }}
-          onClick={onClose}
-        />
-      </Box>
-
-      {/* chat body */}
-      <Box className="chat-body">
-        <Box
-          id="chat-window"
-          sx={{
-            padding: "20px",
-            margin: "5px",
-            display: "flex",
-            flexDirection: "column",
-            gap: 2,
+        {isTyping && <div className=" typing">Assistant is typing...</div>}
+        <Divider />
+        {/* user input section */}
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            askBot(userInput);
           }}
         >
-          {messages.map((msg, index) => (
-            <>
-              <div
-                key={index}
-                style={{
-                  display: "flex",
-                  width: msg.user ? "" : "fit-content",
-                  justifyContent: msg.user ? "flex-end" : "flex-start",
-                  gap: 8,
-                }}
-              >
-                {!msg.user ? (
-                  <Avatar
-                    src={BotIcon}
-                    sx={{
-                      height: 25,
-                      width: 25,
-                      border: "0.5px solid black",
-                      boxShadow: "2px 5px 10px -1px rgba(0, 0, 0, 1)",
-                      // marginBottom:'5px'
-                      marginTop: "15px",
-                      // backgroundColor:'#ede9e8'
-                    }}
-                  />
-                ) : null}
-                {/* <MyLoader/> */}
-                <div>
-                  <Typography
-                    // mt={1}
-                    fontSize={11}
-                    style={{ textAlign: msg.user ? "end" : "start" }}
-                  >
-                    {moment(msg.delivered_at).format("LT")}
-                  </Typography>
-                  <Typography
-                    fontSize={14}
-                    className={msg.user ? "user-msg" : "bot-msg"}
-                  >
-                    {msg.text}
-                  </Typography>
-                </div>
-              </div>
-
-              <div ref={bottomRef}></div>
-            </>
-          ))}
-        </Box>
+          <Box className="chat-footer">
+            <Input
+              type="text"
+              placeholder="Write your message here..."
+              sx={{ width: "80%", marginLeft: "20px" }}
+              disableUnderline
+              value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+              disabled={isDisabled}
+            />
+            <IconButton
+              sx={{ width: "10%", color: "darkslategrey" }}
+              type="submit"
+            >
+              <SendIcon />
+            </IconButton>
+          </Box>
+        </form>
       </Box>
-      {isTyping && <div className=" typing">Assistant is typing...</div>}
-      <Divider />
-      {/* user input section */}
-      <form onSubmit={askBot}>
-        <Box className="chat-footer">
-          <Input
-            type="text"
-            placeholder="Write your message here..."
-            sx={{ width: "80%", marginLeft: "20px" }}
-            disableUnderline
-            value={userInput}
-            onChange={(e) => setUserInput(e.target.value)}
-            // disabled={isDisabled}
-          />
-          <IconButton sx={{ width: "10%", color: "green" }} type="submit">
-            <SendIcon />
-          </IconButton>
-        </Box>
-      </form>
-    </Box>
+    </>
   );
 };
 
